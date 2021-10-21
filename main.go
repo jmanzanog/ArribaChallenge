3e3495998a5740e33c5ed4b3b296514e5eb6a529
jose.manzano <j.manzano.g@hotmail.com>
2021-10-20 19:37:20 -0500

    test deploy changes railway

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 )
-var Router * gin.Engine
+
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello world!",
+			"message": "Hello Jose Manzano!",
 		})
 	})
-	r.Run()
+	var e = r.Run()
+
+	if e != nil {
+		println(e.Error())
+	}
+
 }
