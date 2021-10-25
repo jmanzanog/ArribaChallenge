cb79383351bfb3be1b9925d9978989b84ec2a3b2
jose.manzano <j.manzano.g@hotmail.com>
2021-10-25 11:33:35 -0500

    gin introduce

--- main.go
+++ main.go
@@ -1,20 +1,71 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello Jose Manzano!",
 		})
 	})
-	var e = r.Run()
+
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+
+	// However, this one will match /user/john/ and also /user/john/send
+	// If no other routers match /user/john, it will redirect to /user/john/
+	router.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message)
+	})
+
+	// For each matched request Context will hold the route definition
+	router.POST("/user/:name/*action", func(c *gin.Context) {
+		b := c.FullPath() == "/user/:name/*action" // true
+		c.String(http.StatusOK, "%t", b)
+	})
+
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	router.Use(gin.CustomRecovery(func(c *gin.Context, recovered interface{}) {
+		if err, ok := recovered.(string); ok {
+			c.String(http.StatusInternalServerError, fmt.Sprintf("error: %s", err))
+		}
+		c.AbortWithStatus(http.StatusInternalServerError)
+	}))
+
+	router.Use(gin.Logger())
+
+
+	// Per route middleware, you can add as many as you desire.
+	router.GET("/benchmark", MyBenchLogger(), benchEndpoint)
+
+	router.PUT("/somePut", putting)
+	var e = router.Run()
 
 	if e != nil {
 		println(e.Error())
 	}
+
+}
+
+func benchEndpoint(context *gin.Context) {
+
+}
+
+func MyBenchLogger() gin.HandlerFunc {
+
+	return nil
+}
+
+func putting(context *gin.Context) {
 
 }
